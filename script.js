fae6813b7e104b08bc8b7e623b8c022a7fe9fb95
3sman_63 <om090529@gmail.com>
2026-07-26 17:15:29 +0200

    delete roll loading js

--- script.js
+++ script.js
@@ -1,29 +1,20 @@
-const imageContainerEl = document.querySelector( ".image-container" );
+const imageContainerEl = document.querySelector(".image-container");
 
 
-const btnEl = document.querySelector( ".btn" );
+const btnEl = document.querySelector(".btn");
 
-btnEl.addEventListener( "click", newImages)
+btnEl.addEventListener("click", newImages)
 let index = 3
 let counter = 6
-function newImages ()
-{
-    for ( index ; index < counter ; index++){
-        if (index <= 43) {
-            const newImage = document.createElement( "img" );
-            newImage.src = `Images/Portfolio${ index }.jpg`;
-            imageContainerEl.appendChild( newImage );
-            
+function newImages() {
+    for (index; index < counter; index++) {
+        if (index <= 49) {
+            const newImage = document.createElement("img");
+            newImage.src = `Images/Portfolio${index}.jpg`;
+            imageContainerEl.appendChild(newImage);
+
         }
     }
     counter += 4
 }
-        
-window.addEventListener('load', function() {
-    loadingScreen.style.display = 'none';
-} );
 
-var load = document.getElementById( "loading" );
-function loadfun() {
-    load.style.display = 'none'
-}
